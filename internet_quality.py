b031872b10331decb03730a070482d70c839d58d
Aidan <aidan.jones@montvieux.com>
2018-06-17 15:27:23 +0100

    Output to CSV file with timestamp

--- internet_quality.py
+++ internet_quality.py
@@ -5,6 +5,10 @@ import numpy as np
 import json
 import math
 import csv
+import datetime
+import time
+
+#python ~/repos/internet_quality/internet_quality.py -u admin -p admin -o ~/Documents/test.csv
 
 def speed_test():
     #Uses Speedtest.net servers
@@ -20,8 +24,21 @@ def speed_test():
     return [ping,download_bps,download_mbps,upload_bps,upload_mbps]
 
 def run_speed_test_and_router_check(user,password,output):
-    speed_test_results = speed_test()
-    print(speed_test_results)
+    columns=['time_stamp','ping', 'download_bps', 'download_mbps', 'upload_bps', 'upload_mbps']
+    df = pd.DataFrame(columns=columns)
+
+    for i in range(5):
+        row = [datetime.datetime.now().isoformat()]
+        #speed_test_results = speed_test()
+        speed_test_results = [39.75844383239746, 9370720.147709526, 9.370720147709525, 1098058.9129463232, 1.0980589129463232]
+        row = row + speed_test_results
+        print(speed_test_results)
+        df2 = pd.DataFrame(data = [row], columns=columns)
+        df = df.append(df2,ignore_index=True)
+        df.to_csv(output, index=False, quoting=csv.QUOTE_NONNUMERIC)
+        time.sleep(60 * 10)
+
+    # https://github.com/KreXor/TP-Link-API/blob/master/tplink.py
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Script to record internet speed and router stats")
